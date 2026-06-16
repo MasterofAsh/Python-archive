c3bbb4518a689a5a126b0a7f80f3f3b2cd861551
Master of Ash <aa4070197@gmail.com>
2026-06-16 13:40:56 +0500

    Adding second lesson

---
+++ PythonDay5.py
@@ -0,0 +1,9 @@
+'''
+This is a comment. A comment is initiated with "#" symbol
+These are multiple
+Comments
+'''
+print("My name is Abdullah \n Deez nuts")
+print("I dont like this print statement")
+print("This sentence is an example of escape sequence \n and double quote escape sequence \"Kumulala\" ")
+print("Hey", "Heeeey", "Hi", "The return of hey", sep="|", end="\n Kill me")
