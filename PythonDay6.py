8933b372d8a0f16b8091fa3448b09a46027dd36f
Master of Ash <aa4070197@gmail.com>
2026-06-17 15:15:14 +0500

    Adding third lesson

---
+++ PythonDay6.py
@@ -0,0 +1,26 @@
+Maths = 70
+Physics = 65
+Chemistry = 65
+totalMarks = Maths + Physics + Chemistry
+stdName = "Ahmed Abdullah"
+percentage = 94.56
+
+print(totalMarks)
+print("The type of variable \"totalMarks\" is: ", type(totalMarks))
+print("The type of variable \"stdName\" is: ", type(stdName))
+print("The type of variable \"percentage\" is: ", type(percentage))
+
+complexVar = complex(8, 54)
+print(complexVar)
+
+# List is a collection of different data types in Python, it is also changeable
+stdList = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(stdList)
+
+# Tuple does not allow changes and produces an error
+stdTuple = (("parrot", "sparrow"), (1, 1))
+print(stdTuple)
+
+# Dict data type allows storing values in key-pair sequence
+dict1 = {"Name" : "Umar", "age" : 20, "Passed?" : False}
+print(dict1)
