1e9c295215daa745d5a52c255a0293d4166284a8
Master of Ash <aa4070197@gmail.com>
2026-06-18 14:44:40 +0500

    Adding 4th lesson

---
+++ PythonDay7.py
@@ -0,0 +1,22 @@
+# print(5 + 6) # Addition
+# print(10 - 8) # Subtraction
+# print(20 * 4) # Multiplication
+# print(20 / 5) # Division
+# print(15 // 2) # Floor Division
+# print(15 % 2) # Modulus
+# print(5**2) # Exponential
+
+# Calculator Exercise
+print("C A S I O")
+print("Welcome")
+num1 = 10
+num2 = 5
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Sum of 5 and 7 is: ", addition)
+print("Difference of 5 and 7 is: ", subtraction)
+print("Product of 5 and 7 is: ", multiplication)
+print("Quotient of 5 and 7 is: ", division)
