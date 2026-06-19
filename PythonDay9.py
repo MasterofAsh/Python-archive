91423a5a4c0998cb810db769dbade570ffedb574
Master of Ash <aa4070197@gmail.com>
2026-06-19 12:17:30 +0500

    5th lesson

---
+++ PythonDay9.py
@@ -0,0 +1,12 @@
+a = "2"
+b = "5" 
+print(int(a) + int(b))
+
+# Two types of typecasting; explicit and implicit
+# Explicit: By choice of user
+# Implicit: Done by python interpreter automatically
+
+# Implicit Typecasting example:
+c = 0.9
+d = 9
+print(c + d)
