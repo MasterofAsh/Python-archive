f635340f3aa89f79442ce49b4fbfd0071ce5d1db
Master of Ash <aa4070197@gmail.com>
2026-06-20 13:47:44 +0500

    6th Lesson

---
+++ PythonDay10.py
@@ -0,0 +1,23 @@
+# a = input("Enter your name: ")
+# print("Your name is: ", a)
+
+# x = input("Input first number: ")
+# x = int(x)
+# y = input("Input second number: ")
+# y = int(y)
+# print(x + y)
+
+# Calculate Exercise 2
+
+print(" C A S I O")
+num1 = input("Enter your number: ")
+num1 = int(num1)
+num2 = input("Enter your number: ")
+num2 = int(num2)
+
+print("The sum of ", num1, "and ", num2, "is: ", num1 + num2)
+print("The difference of ", num1, "and ", num2, "is: ", num1 - num2)
+print("The product of ", num1, "and ", num2, "is: ", num1 * num2)
+print("The quotient of ", num1, "and ", num2, "is: ", num1 / num2)
+
+print("Good bye")
