c8280fbbbf79f9c5f3b694894ed3a9e4487f2e84
Master of Ash <aa4070197@gmail.com>
2026-06-21 15:11:03 +0500

    7th lesson

---
+++ PythonDay11.py
@@ -0,0 +1,17 @@
+name = "Harry"
+friend = "Rohan"
+anotherFriend = 'Lovish'
+#string datatype can be addressed with "" and '' both.
+print("Hello", name)
+
+print("He said, \"I want to eat an apple\"")
+print('He said, "Please Kill me"')
+yogurt = """
+yogurt: what
+gurt: yo
+"""
+print(yogurt)
+print(name[2])
+# Printing each character of a string using a for loop
+for character in yogurt:
+    print(character)
