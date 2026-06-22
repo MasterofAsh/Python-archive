9bd472e2eafdf00d378a6120c508f58628ef9526
Master of Ash <aa4070197@gmail.com>
2026-06-22 10:50:43 +0500

    8th Lesson

---
+++ PythonDay12.py
@@ -0,0 +1,15 @@
+name = "Abdullah, Umar"
+fruit = "Strawberry"
+print(len(name))
+print("Strawberry is a", (len(fruit)), "letter word")
+print(name[0:8])
+
+print(fruit[0:4])
+print(fruit[:])
+print(fruit[:10])
+print(fruit[0:-5])
+print(fruit[-3:-2])
+
+#Quiz
+name2 = "Harry"
+print(name2[-4:-2])
