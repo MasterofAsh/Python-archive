8fa294fec9be27a8b19eb74b750fa39a567cc5e9
Master of Ash <aa4070197@gmail.com>
2026-06-23 13:45:36 +0500

    9th Lesson

---
+++ PythonDay13.py
@@ -0,0 +1,42 @@
+a = "Abdullah@ Umar@@ Annie@@@"
+print(a.upper())
+
+print(a.lower())
+
+print(a.rstrip("@"))
+
+print(a.replace("Harry", "Abdullah"))
+
+print(a.split(" "))
+
+myBlog = "weLcome to my blog"
+print(myBlog.capitalize())
+
+print(myBlog.center(50))
+
+print(a.count("A"))
+
+print(a.endswith("@"))
+
+print(len(a))
+print(a.endswith("Umar@@", 8, 16))
+
+print(a.find("Umar"))
+
+print(a.isalnum())
+
+print(a.isalpha())
+
+print(a.islower())
+
+print(a.isprintable())
+
+print(a.isspace())
+
+print(a.istitle())
+
+print(myBlog.startswith("e"))
+
+print(myBlog.swapcase())
+
+print(myBlog.title())
