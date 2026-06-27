f486fbfdd553c72d6d1a0c6d5e6a0141223a6a07
Master of Ash <aa4070197@gmail.com>
2026-06-27 14:09:35 +0500

    13th Lesson

---
+++ PythonDay17.py
@@ -0,0 +1,15 @@
+name = "abdullah"
+for i in name:
+    print(i)
+
+colors = ["red", "yellow", "green", "blue"]
+for i in colors:
+    print(i)
+    for x in i:
+        print(x)
+
+for k in range(-1, 50):
+    print(k)
+
+for w in range(0, 50, 5):
+    print(w)
