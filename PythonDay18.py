95710a321668f61e2f87761006927fd2e21d8978
Master of Ash <aa4070197@gmail.com>
2026-06-28 16:08:32 +0500

    14th Lesson

---
+++ PythonDay18.py
@@ -0,0 +1,21 @@
+# for i in range(3):
+#     print(i)
+
+# i = 0
+# while (i < 38):
+#     i = int(input("Enter your desired number: "))
+#     print(i)
+
+# num = 5
+# while (num > 0):
+#     print(num)
+#     num = num - 1
+
+# Emulating Do-while loop
+
+x = 1
+while (x <= 2):
+    print(x)
+    x = x + 1
+    if (x == 2):
+        print("Breaking the while loop")
