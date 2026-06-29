81390bd517a2021a08ec608678687e872483690a
Master of Ash <aa4070197@gmail.com>
2026-06-29 15:14:00 +0500

    Actual 15th Lesson

---
+++ PythonDay19.py
@@ -0,0 +1,22 @@
+# for i in range(12):
+#     print("5 x ", i + 1 , "=", (i + 1) * 5)
+#     if (i == 5):
+#         break
+# print("Loop has been broken")
+
+# w = "sigma"
+# while (w == "sigma"):
+#     e = input("Write your name: ")
+#     if (e == "skibidi"):
+#         break
+#     else:
+#         print("Very sigma")
+# print("Kumulala kumulala")
+
+i = 0
+while True:
+    print(i)
+    i += 1
+    if (i%100 == 0):
+        break
+print("Sugoma")
