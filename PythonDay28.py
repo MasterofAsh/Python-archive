96e30b97d7d697b6edff2e60c6b74beb9d4d91da
Master of Ash <aa4070197@gmail.com>
2026-07-09 12:22:35 +0500

    24th Lesson

---
+++ PythonDay28.py
@@ -0,0 +1,11 @@
+# f-string allows us to use variables in a string
+
+name = "Ahmed Abdullah"
+country = "Pakistan"
+
+print(f"My name is {name} and I am from {country}")
+
+price = 69420
+item = ["carrot", "mango"]
+
+print(f"Would you like a {item[1]} for {price} rupees.")
