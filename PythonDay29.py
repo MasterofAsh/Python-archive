ad9ee908431f5f6318e4688cd1bc05b6992c4716
Master of Ash <aa4070197@gmail.com>
2026-07-10 10:42:06 +0500

    25th Lesson

---
+++ PythonDay29.py
@@ -0,0 +1,11 @@
+def square(n):
+    """
+    Takes in a number 'n', and the returns the square of n
+    """
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+# Docstrings are used to document use of functions in your code, they are done right in the start of the function
+
+import this
