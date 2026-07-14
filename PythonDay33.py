948311f69790eca5043654b050f2ac3bdc72e253
Master of Ash <aa4070197@gmail.com>
2026-07-14 12:22:11 +0500

    29th Lesson

---
+++ PythonDay33.py
@@ -0,0 +1,12 @@
+# Dictionaries
+myDict = {
+    "Abdullah" : "17 year old boy",
+    "Nigger" : "A black person",
+    "Age of Nigger" : 17,
+    "Elligible" : False
+}
+print(myDict["Abdullah"])
+print(myDict.keys())
+
+for key in myDict.keys():
+    print(key)
