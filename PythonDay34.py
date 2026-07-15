9323737db053a0f8522ce1fcb7571173330fd017
Master of Ash <aa4070197@gmail.com>
2026-07-15 11:08:47 +0500

    30th Lesson

---
+++ PythonDay34.py
@@ -0,0 +1,13 @@
+# Dictionary Methods
+
+eP = {1 : 64, 2 : 69, 3: 79, 4 : 94, 5 : 45}
+eP2 = {6 : 34, 7: 95, 8: 88, 9 : 40, 10 : 66}
+
+eP.update(eP2)
+
+eP.pop(10)
+print(eP)
+
+eP.clear()
+print(eP)
+# Many of the methods that can be applied in sets can also be applied to dictionaries
