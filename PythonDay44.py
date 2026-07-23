40093f9163410f9d16099a0a73c80360df7b7f1f
Master of Ash <aa4070197@gmail.com>
2026-07-23 11:59:30 +0500

    38th Lesson

---
+++ PythonDay44.py
@@ -0,0 +1,14 @@
+from math import sqrt, pi
+# import math as m
+import math
+
+print(math.factorial(5))
+print(sqrt(25))
+print(pi)
+
+print(dir(math))
+print(type(math.nan))
+
+from testAbdullah import Abdullah, a
+Abdullah()
+print(a)
