d1a8f060394aec67ae6f2a7dc3047e2eed305764
Master of Ash <aa4070197@gmail.com>
2026-07-28 20:42:36 +0500

    43rd Lesson

---
+++ PythonDay50.py
@@ -0,0 +1,21 @@
+# f = open('testAbdullah2.txt', "r")
+# i = 0
+# while True:
+#     i = i + 1
+#     line = f.readline()
+#     if not line:
+#         break
+#     m1 = int(line.split(",")[0])
+#     m2 = int(line.split(",")[1])
+#     m3 = int(line.split(",")[2])
+
+#     print(f"Marks of student {i} in maths is: {m1}")
+#     print(f"Marks of student {i} in english is: {m2}")
+#     print(f"Marks of student {i} in urdu is: {m3}")
+#     print(line)
+
+
+f = open('testAbdullah2.txt', 'w')
+lines = ['line 1\n', 'line 2\n', 'line 3\n']
+f.writelines(lines)
+f.close()
