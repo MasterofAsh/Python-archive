c33dd599fcddbb9f493f4a6d9a6b9c6d55d8a91e
MasterofAsh <aa4070197@gmail.com>
2026-09-07 20:53:42 +0500

    73rd Lesson

---
+++ PythonDay85.py
@@ -0,0 +1,31 @@
+import argparse
+import requests
+
+def download_file(url, local_filename):
+    if (local_filename is None):
+        local_filename = url.split('/')[-1]
+
+    with requests.get(url, stream = True) as r:
+        r.raise_for_status()
+        with open(local_filename, "wb") as f:
+            for chunk in r.iter_content(chunk_size = 8192):
+                f.write(chunk)
+    return local_filename
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("url", help = "Url of the file to download")
+
+parser.add_argument("-o", "--output", help = "Name of the file", default = None)
+
+args = parser.parse_args()
+
+print(args.url)
+print(args.output, type(args.output))
+download_file(args.url, args.output)
+
+
+# TO run, type out in the terminal:
+# 'python "Your Python filename.py" "your .jpg image link" -o "the name you want your file saved as.jpg"
+# Example:
+# python PythonDay85.py https://imagej.net/image/Cell_Colony2.jpg -o mycell.jpg
