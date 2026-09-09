2860f0af4b74207fa304656e271ee9d3d4312114
MasterofAsh <aa4070197@gmail.com>
2026-09-09 17:01:40 +0500

    74th lesson

---
+++ PythonDay86.py
@@ -0,0 +1,18 @@
+# numbers = [1, 2, 3, 4, 5]
+
+# while (n := len(numbers)) > 0:
+#     print(numbers.pop())
+
+happy = True
+
+print(happy)
+
+foods = list()
+while True:
+    food = input("What food do you like?\n")
+
+    if food == "Quit":
+        break
+
+    foods.append(food)
+
